df965cedba9a7b21ceedc1e4a784497b90ffa7c0
Ricketson Palacio <Ricketson.king@gmail.com>
2026-07-19 14:43:54 +0800

    feat(main): initialize Supabase and dotenv in main function

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,19 @@
 import 'package:blog_app/app/app.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await dotenv.load();
+
+  await Supabase.initialize(
+    url: dotenv.env['SUPABASE_URL']!,
+    publishableKey: dotenv.env['SUPABASE_ANON_KEY']!,
+  );
+
   runApp(const App());
 }
+
+final SupabaseClient supabase = Supabase.instance.client;
