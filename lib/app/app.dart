6d15bd66959d5195ea44b9bd60f60bc0ebbf6643
Ricketson Palacio <Ricketson.king@gmail.com>
2026-07-21 00:21:47 +0800

    feat(app): update App widget to use GoRouter for navigation

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,17 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 import 'package:blog_app/app/app_theme.dart';
 
 class App extends StatelessWidget {
-  const App({super.key});
+  const App({required this.router, super.key});
+
+  final GoRouter router;
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
       title: 'The Journal',
       debugShowCheckedModeBanner: false,
       theme: AppTheme.light,
-      home: const Placeholder(),
+      routerConfig: router,
     );
   }
 }
