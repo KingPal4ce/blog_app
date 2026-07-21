1945803d7a508b183383b3ee6e2787090f5ce36a
Ricketson Palacio <Ricketson.king@gmail.com>
2026-07-22 00:24:47 +0800

    feat(main): add PostsProvider to MultiProvider for post management

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
+import 'package:provider/single_child_widget.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 import 'package:blog_app/app/app.dart';
 import 'package:blog_app/app/router.dart';
 import 'package:blog_app/providers/auth_provider.dart';
+import 'package:blog_app/providers/posts_provider.dart';
 import 'package:blog_app/services/auth_service.dart';
+import 'package:blog_app/services/posts_service.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -20,12 +23,14 @@ Future<void> main() async {
   );
 
   final AuthProvider authProvider = AuthProvider(AuthService());
+  final PostsProvider postsProvider = PostsProvider(PostsService());
   final GoRouter router = buildRouter(authProvider);
 
   runApp(
     MultiProvider(
-      providers: <ChangeNotifierProvider<AuthProvider>>[
+      providers: <SingleChildWidget>[
         ChangeNotifierProvider<AuthProvider>.value(value: authProvider),
+        ChangeNotifierProvider<PostsProvider>.value(value: postsProvider),
       ],
       child: App(router: router),
     ),
